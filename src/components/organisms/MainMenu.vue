ad60855dab0beee4d031c51456bec11a9a2126ee
vebrovski <6883519+vebrovski@users.noreply.github.com>
2021-02-03 14:21:41 +0100

    Main menu component added.

---
+++ src/components/organisms/MainMenu.vue
@@ -0,0 +1,23 @@
+<template>
+  <menu-list class="main-menu flex flex-row" :routes="mainMenuRoutes"></menu-list>
+</template>
+
+<script>
+import mainMenuRoutes from "@router/mainMenuRoutes"
+import MenuList from "@components/molecules/MenuList.vue"
+
+export default {
+  name: "MainMenu",
+  components: { MenuList },
+
+  data () {
+    return {
+      mainMenuRoutes
+    }
+  }
+}
+</script>
+
+<style scoped>
+
+</style>
