bb642442c8c87bf2febd8e16b4ab1c9a5afd0dbc
vebrovski <6883519+vebrovski@users.noreply.github.com>
2021-02-03 12:05:34 +0100

    Added parent wrapper and its classes. Reformat code.

--- src/components/App.vue
+++ src/components/App.vue
@@ -1,13 +1,14 @@
 <template>
-  <router-view></router-view>
+  <div class="bg-black text-white h-screen p-4">
+    <router-view></router-view>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'App'
-  }
+export default {
+  name: "App",
+};
 </script>
 
 <style lang="scss" scoped>
-
 </style>
