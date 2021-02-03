96579860214bbf693b56713f9d45e4de5ad5dbb1
vebrovski <6883519+vebrovski@users.noreply.github.com>
2021-02-03 11:33:31 +0100

    Added initial pages.

---
+++ src/components/pages/PageHome.vue
@@ -0,0 +1,19 @@
+<template>
+  <page>
+    <row>
+    </row>
+  </page>
+</template>
+
+<script>
+  import Page from "@components/templates/Page.vue";
+  import Row from "@components/templates/Row.vue";
+
+  export default {
+    name: "PageHome",
+    components: { Row, Page },
+  }
+</script>
+
+<style scoped>
+</style>
---
+++ src/components/pages/PageMovies.vue
@@ -0,0 +1,16 @@
+<template>
+  <page></page>
+</template>
+
+<script>
+import Page from '@components/templates/Page.vue'
+
+export default {
+  name: "PageMovies",
+  components: { Page },
+}
+</script>
+
+<style scoped>
+
+</style>
