95183547287fd7e25ca482b077aea393778ceffd
vebrovski <6883519+vebrovski@users.noreply.github.com>
2021-02-04 17:53:03 +0100

    Title component added.

---
+++ src/components/atoms/Title.vue
@@ -0,0 +1,24 @@
+<!-- No template because of render function -->
+
+<script>
+export default {
+  name: "Title",
+
+  props: {
+    level: {
+      type: Number,
+      default: 1,
+    },
+  },
+
+  render(createElement) {
+    return createElement(
+      "h" + this.level, // tag name
+      this.$slots.default // array of children
+    );
+  },
+};
+</script>
+
+<style scoped>
+</style>
