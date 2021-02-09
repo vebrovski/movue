a6467bedbd9e8899873e7315a713569d858af43d
vebrovski <6883519+vebrovski@users.noreply.github.com>
2021-02-09 09:37:27 +0100

    Poster component added.

---
+++ src/components/molecules/Poster.vue
@@ -0,0 +1,33 @@
+<template>
+  <div class="poster">
+    <Images class="poster__image" :path="path"></Images>
+    <slot></slot>
+  </div>
+</template>
+
+<script>
+  import Images from "@components/atoms/Images.vue";
+
+  export default {
+    name: "Poster",
+
+    components: {
+      Images
+    },
+
+    props: {
+      path: {
+        type: String,
+        required: true
+      },
+      link: {
+        type: String,
+      },
+      width: '',
+      height: ''
+    }
+  }
+</script>
+
+<style scoped lang="scss">
+</style>
