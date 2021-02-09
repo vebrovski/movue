2b9c492fd21df43ecb34602ad69adbd9756b9693
vebrovski <6883519+vebrovski@users.noreply.github.com>
2021-02-09 10:27:25 +0100

    Date component added. Displaying date from dateFormat module.

---
+++ src/components/molecules/Date.vue
@@ -0,0 +1,24 @@
+<template>
+  <div class="date" v-if="date && date.length > 0">
+    {{ dateFormatted }}
+  </div>
+</template>
+
+<script>
+  import {dateFormat} from "@src/utils/dateFormat";
+
+  export default {
+    name: "Date",
+
+    props: [
+      'date',
+      'format'
+    ],
+
+    computed: {
+      dateFormatted() {
+        return dateFormat(this.date, this.format);
+      }
+    },
+  }
+</script>
