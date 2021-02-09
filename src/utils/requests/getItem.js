06a0aaac288053f529d3069216c1384ca9c4ad27
vebrovski <6883519+vebrovski@users.noreply.github.com>
2021-02-09 17:39:57 +0100

    Removed Promise anti-pattern. request module already returns Promise.

--- src/utils/requests/getItem.js
+++ src/utils/requests/getItem.js
@@ -10,28 +10,22 @@ const api = new MovueApi();
  * @param listType
  * @returns {Promise} Promise object represents data from api call.
  */
-const getItem = async (mediaType, id, listType = '') => {
-    const mediaTypes = (['tv', 'movie', 'person']);
+const getItem = (mediaType, id, listType = '') => {
+  const mediaTypes = (['tv', 'movie', 'person']);
 
-    return new Promise((resolve, reject) => {
-        if (!mediaTypes.includes(mediaType)) {
-            reject(`Only ${mediaTypes.map(type => `"${type}"`).join(', ')} media types are allowed.`);
-        }
+  if (!mediaTypes.includes(mediaType)) {
+    throw new Error(`Only ${mediaTypes.map(type => `"${type}"`).join(', ')} media types are allowed.`);
+  }
 
-        if (listType) {
-            resolve(
-                request(`${api.baseUrl}/${api.version}/${mediaType}/${id}/${listType}?api_key=${api.getApiKey()}`)
-                    .then(response => response.data)
-                    .catch(reason => reject(reason))
-            )
-        } else {
-            resolve(
-                request(`${api.baseUrl}/${api.version}/${mediaType}/${id}?api_key=${api.getApiKey()}`)
-                    .then(response => response.data)
-                    .catch(reason => reject(reason))
-            )
-        }
-    });
+  if (listType) {
+    return request(`${api.baseUrl}/${api.version}/${mediaType}/${id}/${listType}?api_key=${api.getApiKey()}`)
+      .then(response => response.data)
+      .catch(reason => reject(reason))
+  } else {
+    return request(`${api.baseUrl}/${api.version}/${mediaType}/${id}?api_key=${api.getApiKey()}`)
+      .then(response => response.data)
+      .catch(reason => reject(reason))
+  }
 };
 
 export default getItem;
