5a84781d362538bb3a15f2383740e1666684ad64
vebrovski <6883519+vebrovski@users.noreply.github.com>
2021-02-11 15:15:44 +0100

    Props default values, reformat.

--- src/components/molecules/Poster.vue
+++ src/components/molecules/Poster.vue
@@ -1,33 +1,40 @@
 <template>
   <div class="poster">
-    <Images class="poster__image" :path="path"></Images>
+    <images 
+      :path="path" 
+      class="poster__image"
+    />
     <slot></slot>
   </div>
 </template>
 
 <script>
-  import Images from "@components/atoms/Images.vue";
+import Images from "@components/atoms/Images.vue";
 
-  export default {
-    name: "Poster",
+export default {
+  name: "Poster",
 
-    components: {
-      Images
+  components: {
+    Images,
+  },
+
+  props: {
+    path: {
+      type: String,
+      required: true,
     },
-
-    props: {
-      path: {
-        type: String,
-        required: true
-      },
-      link: {
-        type: String,
-      },
-      width: '',
-      height: ''
-    }
-  }
+    link: {
+      type: String,
+      default: "",
+    },
+    width: {
+      type: String,
+      default: "",
+    },
+    height: {
+      type: String,
+      default: "",
+    },
+  },
+};
 </script>
-
-<style scoped lang="scss">
-</style>
