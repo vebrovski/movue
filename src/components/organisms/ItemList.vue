35eb23f54fd8036ab8515d734c002399e3e4ab57
vebrovski <6883519+vebrovski@users.noreply.github.com>
2021-02-12 14:48:50 +0100

    Initial component for item list added.

---
+++ src/components/organisms/ItemList.vue
@@ -0,0 +1,33 @@
+<template>
+  <list>
+    <list-item
+      v-for="item in items" 
+      :key="item.id"
+    >
+      <Card
+        :title="item.title"
+        :image="item.poster_path || item.backdrop_path"
+        :score="item.vote_average"
+      ></Card>
+    </list-item>
+  </list>
+</template>
+
+<script>
+import List from '@components/molecules/List.vue';
+import ListItem from '@components/molecules/ListItem.vue';
+import Card from "@components/molecules/Card.vue";
+
+export default {
+  name: "ItemList",
+
+  components: { List, ListItem, Card },
+
+  props: {
+    items: {
+      type: Array,
+      default: () => []
+    },
+  },
+};
+</script>
