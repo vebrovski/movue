1e4eb10a54704d72a7d1ca62f7a4129a83e2058c
vebrovski <6883519+vebrovski@users.noreply.github.com>
2021-02-18 16:58:48 +0100

    Added button component.

---
+++ src/components/atoms/ButtonEl.vue
@@ -0,0 +1,28 @@
+<template>
+  <button 
+    :type="type" 
+    class="button"
+  >
+    {{ text }}
+  </button>
+</template>
+
+<script>
+export default {
+  name: "ButtonEl",
+
+  props: {
+    type: {
+      type: String,
+      default: "submit",
+    },
+    text: {
+      type: String,
+      default: "Submit",
+    },
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
