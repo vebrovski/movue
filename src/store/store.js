d902fab2755ca81cd97f9adddcf4c562ac48707d
vebrovski <6883519+vebrovski@users.noreply.github.com>
2021-02-19 18:05:02 +0100

    Added store for popularItems.

--- src/store/store.js
+++ src/store/store.js
@@ -1,14 +1,24 @@
 import Vue from 'vue'
 import Vuex from 'vuex'
+import { getItems } from '@utils/requests/getItems'
 
 Vue.use(Vuex)
 
 export default new Vuex.Store({
   state: {
+    popularItems: []
   },
   mutations: {
+    setPopularItems(state, payload) {
+      state.popularItems = payload
+    }
   },
   actions: {
+    getPopularItems(context, payload) {
+      getItems(payload.mediaType, payload.listType).then(response => {
+        context.commit('setPopularItems', response.data.results)
+      })
+    }
   },
   modules: {
   }
