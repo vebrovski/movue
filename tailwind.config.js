47295803e9bf8feddcfbcaa1ae2e41bdcf77b47f
vebrovski <6883519+vebrovski@users.noreply.github.com>
2021-02-22 10:30:10 +0100

    Removing unused styles config.

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,5 +1,7 @@
 module.exports = {
-  purge: ['./index.html', './src/**/*.{vue,js,ts,jsx,tsx}'],
+  purge: [
+    './src/**/*.{html,vue,js,ts,jsx,tsx}'
+  ],
   darkMode: false, // or 'media' or 'class'
   theme: {
     extend: {},
