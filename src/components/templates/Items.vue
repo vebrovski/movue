51b7cee34a9087af278efb12649ccf545417ec3b
vebrovski <6883519+vebrovski@users.noreply.github.com>
2021-02-23 13:10:57 +0100

    Template component for items added.

---
+++ src/components/templates/Items.vue
@@ -0,0 +1,18 @@
+<template>
+  <div class="items">
+    <slot>
+      <DefaultSlot></DefaultSlot>
+    </slot>
+  </div>
+</template>
+
+<script>
+import DefaultSlot from "@components/templates/DefaultSlot.vue";
+export default {
+  name: "Items",
+  components: { DefaultSlot },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
