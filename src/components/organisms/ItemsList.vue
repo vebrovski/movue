6ee1f28b7d7f0c7adc6954b48e12f7cb6aa37070
vebrovski <6883519+vebrovski@users.noreply.github.com>
2021-02-23 14:01:27 +0100

    Reformat and rename.

--- src/components/organisms/ItemsList.vue
+++ src/components/organisms/ItemsList.vue
@@ -1,32 +1,32 @@
 <template>
-  <list>
-    <list-item
-      v-for="item in items" 
+  <List>
+    <ListItem
+      v-for="item in items"
       :key="item.id"
     >
       <Card
         :title="item.title"
         :image="item.poster_path || item.backdrop_path"
         :score="item.vote_average"
-      ></Card>
-    </list-item>
-  </list>
+      />
+    </ListItem>
+  </List>
 </template>
 
 <script>
-import List from '@components/molecules/List.vue';
-import ListItem from '@components/molecules/ListItem.vue';
+import List from "@components/molecules/List.vue";
+import ListItem from "@components/molecules/ListItem.vue";
 import Card from "@components/molecules/Card.vue";
 
 export default {
-  name: "ItemList",
+  name: "ItemsList",
 
   components: { List, ListItem, Card },
 
   props: {
     items: {
       type: Array,
-      default: () => []
+      default: () => [],
     },
   },
 };
