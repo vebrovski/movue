85cd91d90ccdbf4fbd88341661d4ced16639dab4
vebrovski <6883519+vebrovski@users.noreply.github.com>
2021-03-01 12:04:59 +0100

    Removed global Swiper import and added it directly to component.

--- src/components/organisms/ItemsSlider.vue
+++ src/components/organisms/ItemsSlider.vue
@@ -1,10 +1,10 @@
 <template>
   <div 
-    v-swiper:ItemListSwiper="{ swiperOption }" 
-    class="slider"
+    class="slider" 
+    :class="{ 'swiper-container': enableDefaultSlider }"
   >
-    <List 
-      class="slider__items" 
+    <List
+      class="slider__items"
       :class="{ 'swiper-wrapper': enableDefaultSlider }"
     >
       <ListItem
@@ -22,18 +22,23 @@
         </slot>
       </ListItem>
     </List>
-    <div 
-      :class="{ 'swiper-button-next': enableDefaultSlider }" 
+    <div
+      :class="{ 'swiper-button-next': enableDefaultSlider }"
       class="button-next"
     ></div>
-    <div 
-      :class="{ 'swiper-button-prev': enableDefaultSlider }" 
+    <div
+      :class="{ 'swiper-button-prev': enableDefaultSlider }"
       class="button-prev"
     ></div>
   </div>
 </template>
 
 <script>
+import Swiper from "swiper";
+import SwiperCore, { Navigation, Pagination } from "swiper/core";
+SwiperCore.use([Navigation, Pagination]);
+import "swiper/swiper-bundle.css"; // (>= Swiper 6.x)
+
 import List from "@components/molecules/List.vue";
 import ListItem from "@components/molecules/ListItem.vue";
 import Card from "@components/molecules/Card.vue";
@@ -73,7 +78,7 @@ export default {
   components: {
     ListItem,
     List,
-    Card
+    Card,
   },
 
   props: {
@@ -87,45 +92,56 @@ export default {
     },
   },
 
-  data() {
-    return {
-      swiperOption: {}
-    }
+  computed: {
   },
 
   watch: {
-    items(value) {
-      this.swiperOption = {
-        spaceBetween: 30,
+    items: {
+      handler(value) {
+        if (value.length > 0) {
+          this.sliderInit();
+        }
+      },
+    },
+  },
+
+  methods: {
+    sliderInit() {
+      new Swiper(".swiper-container", {
+        spaceBetween: 10,
         slidesPerView: 7,
-        loop: true,
-        autoHeight: true,
+        slidesPerGroup: 7,
+        observer: true,
+        observeParents: true,
+        rebuildOnUpdate: true,
         navigation: {
-          nextEl: '.swiper-button-next',
-          prevEl: '.swiper-button-prev',
+          nextEl: ".swiper-button-next",
+          prevEl: ".swiper-button-prev",
         },
         breakpoints: {
-          640: {
+          0: {
             slidesPerView: 3,
-            spaceBetween: 20,
+            spaceBetween: 10,
+            navigation: false,
           },
           768: {
             slidesPerView: 4,
-            spaceBetween: 40,
+            spaceBetween: 10,
           },
           1024: {
             slidesPerView: 5,
-            spaceBetween: 50,
+            spaceBetween: 10,
           },
           1280: {
             slidesPerView: 7,
-            spaceBetween: 50,
-          }
-        }
-      }
-    }
-  } 
-}
+            slidesPerGroup: 7,
+            spaceBetween: 10,
+          },
+        },
+      });
+    },
+  },
+};
 </script>
 
 <style scoped lang="scss">
--- src/index.js
+++ src/index.js
@@ -5,11 +5,6 @@ import store from '@store/store'
 import './index.scss'
 import MenuList from "@components/molecules/MenuList.vue"
 
-import VueAwesomeSwiper from 'vue-awesome-swiper'
-import 'swiper/swiper-bundle.css' // (>= Swiper 6.x)
-//import 'swiper/css/swiper.css' // (<= Swiper 5.x)
-Vue.use(VueAwesomeSwiper)
-
 Vue.config.productionTip = false
 
 Vue.component('MenuList', MenuList)
