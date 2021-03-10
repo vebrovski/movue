083cab5895c8e4ba1ec450d9b2a4135aadfd6ef6
vebrovski <6883519+vebrovski@users.noreply.github.com>
2021-03-10 10:16:10 +0100

    Removed specific component for popular items and made Items component more generic.

--- src/components/templates/Items.vue
+++ src/components/templates/Items.vue
@@ -1,15 +1,106 @@
 <template>
-  <div class="items">
-    <slot>
-      <DefaultSlot></DefaultSlot>
-    </slot>
+  <div 
+    class="items"
+    :class="`${listType}`"
+  >
+    <ItemsList 
+      v-if="templateType === 'list'" 
+      :items="items" 
+      class="popular-list"
+      :class="`${listType}-list`"
+    />
+    <ItemsSlider 
+      v-if="templateType === 'slider'" 
+      :items="items" 
+      :class="`${listType}-slider`"
+    />
   </div>
 </template>
 
 <script>
-import DefaultSlot from "@components/templates/DefaultSlot.vue";
+import { getItems } from '@utils/requests/getItems'
+import Cache from '@utils/cache';
+import ItemsSlider from '@components/organisms/ItemsSlider.vue';
+import ItemsList from '@components/organisms/ItemsList.vue';
+
 export default {
   name: "Items",
-  components: { DefaultSlot },
+
+  components: {
+    ItemsSlider,
+    ItemsList
+  },
+
+  props: {
+    /**
+     * Config for targeting media type.
+     * @value movie, tv, people
+     */
+    mediaType: {
+      type: String,
+      default: "tv",
+    },
+
+    /**
+     * Config for targeting list type.
+     * @value for movies - latest, now_playing, popular, top_rated, upcoming.
+     * @value for tv - latest, airing_today, on_the_air, popular, top_rated.
+     * @value for people - latest, popular.
+     */
+    listType: {
+      type: String,
+      default: 'popular'
+    },
+
+    /**
+     * Config for display of items.
+     * @value list, slider, grid (to be added).
+     */
+    templateType: {
+      type: String,
+      default: 'list'
+    }
+  },
+
+  data() {
+    return {
+      items: [],
+      cache: new Cache(),
+    }
+  },
+
+  watch: {
+    mediaType() {
+      this.getItemsLocal(this.mediaType, this.listType)
+    }
+  },
+
+  mounted() {
+    this.getItemsLocal(this.mediaType, this.listType)
+  },
+
+  destroyed() {
+    this.cache.clear();
+  },
+
+  methods: {
+    getItemsLocal(mediaType, listType) {
+      if (this.cache.exists(mediaType)) {
+        this.items = this.cache.getItem(mediaType);
+      } else {
+        getItems(mediaType, listType)
+        .then(response => {
+          this.items = response.data.results
+          this.cache.setItem(mediaType, this.items);
+        })
+        .catch((reason) => {
+          alert(reason);
+        });
+      }
+    },
+  },
 };
 </script>
+
+<style scoped lang="scss">
+</style>
