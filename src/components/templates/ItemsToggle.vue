e576e24c8d2d4f74093e915f0e3ccd146a3c5763
vebrovski <6883519+vebrovski@users.noreply.github.com>
2021-03-10 12:13:30 +0100

    Generic items toggle initial component. Removed specific popular toggle component.

---
+++ src/components/templates/ItemsToggle.vue
@@ -0,0 +1,81 @@
+<template>
+  <div class="items-toggle">
+    <div class="items-toggle__header">
+      <TitleEl :level="2">
+        {{ title }}
+      </TitleEl>
+      <Toggle
+        :labels="toggleLabels"
+        :values="toggleValues"
+        @toggle="toggle"
+      ></Toggle>
+    </div>
+    <div class="items-toggle__content">
+      <slot>
+        <Items
+          :media-type="mediaType || defaultMediaType" 
+          :list-type="listType" 
+          :template-type="templateType"
+        />
+      </slot>
+    </div>
+  </div>
+</template>
+
+<script>
+import TitleEl from '@components/atoms/TitleEl.vue';
+import Toggle from "@components/molecules/Toggle.vue";
+import Items from '@components/templates/Items.vue';
+
+export default {
+  name: "ItemsToggle",
+
+  components: {
+    TitleEl,
+    Toggle,
+    Items
+  },
+
+  props: {
+    title: {
+      type: String,
+      default: ''
+    },
+    defaultMediaType: {
+      type: String,
+      default: 'tv'
+    },
+    listType: {
+      type: String,
+      default: 'popular'
+    },
+    templateType: {
+      type: String,
+      default: 'list'
+    },
+    toggleLabels: {
+      type: Array,
+      default: () => ['Label 1', 'Label 2'],
+    },
+    toggleValues: {
+      type: Array,
+      default: () => ['Value 1', 'Value 2'],
+    },
+  },
+
+  data() {
+    return {
+      mediaType: this.defaultMediaType
+    }
+  },
+
+  methods: {
+    toggle(value) {
+      this.mediaType = value;
+    }
+  },
+};
+</script>
+
+<style scoped lang="scss">
+</style>
