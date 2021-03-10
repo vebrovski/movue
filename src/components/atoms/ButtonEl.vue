d9f6fc1d3ff7ae7dd061f6ae7a94d8a09ceb488e
vebrovski <6883519+vebrovski@users.noreply.github.com>
2021-03-10 12:06:24 +0100

    Button styling.

--- src/components/atoms/ButtonEl.vue
+++ src/components/atoms/ButtonEl.vue
@@ -25,4 +25,25 @@ export default {
 </script>
 
 <style scoped lang="scss">
+.button {
+  position: relative;
+  display: flex;
+  width: 100%;
+  align-content: center;
+  align-items: center;
+  justify-content: center;
+  padding: 0.5rem 1.5rem;
+  border: 2px solid  $border-color;
+  margin-right: 1rem;
+  background-color: transparent;
+  border-radius: $border-radius;
+  color: white;
+  font-size: 1.6rem;
+  font-weight: $font-weight-semi-bold;
+
+  &:hover,
+  &:active {
+    border-color: $red-color;
+  }
+}
 </style>
