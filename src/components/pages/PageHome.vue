d5e1b150590885a2d7c79669e03efcdf581bfe0f
vebrovski <6883519+vebrovski@users.noreply.github.com>
2021-03-11 08:35:39 +0100

    Popular items on home page added.

--- src/components/pages/PageHome.vue
+++ src/components/pages/PageHome.vue
@@ -1,19 +1,61 @@
 <template>
   <page>
     <row>
+      <container>
+        <ItemsToggle 
+          title="What's popular" 
+          template-type="slider"
+          :config="popularItemsConfig"
+        />
+      </container>
+    </row>
+    <row>
+      <container>
+      </container>
     </row>
   </page>
 </template>
 
 <script>
-  import Page from "@components/templates/Page.vue";
-  import Row from "@components/templates/Row.vue";
+import Page from "@components/templates/Page.vue";
+import Row from "@components/templates/Row.vue";
+import Container from '@components/templates/Container.vue';
+import ItemsToggle from '@components/templates/ItemsToggle.vue';
 
-  export default {
-    name: "PageHome",
-    components: { Row, Page },
-  }
+export default {
+  name: "PageHome",
+
+  components: { 
+    Page, 
+    Row,
+    Container,
+    ItemsToggle,
+  },
+
+  data() {
+    return {
+      popularItemsConfig: [
+        {
+          label: 'Series',
+          value: 'tv',
+          list: 'popular'
+        },
+        {
+          label: 'Movies',
+          value: 'movie',
+          list: 'now_playing'
+        },
+        {
+          label: 'Person',
+          value: 'person',
+          list: 'popular'
+        }
+      ]
+    }
+  },
+};
 </script>
 
 <style scoped>
 </style>
+
